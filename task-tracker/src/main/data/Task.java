0dac656f1974c56c2d233bdde4fbd491df7bc7dd
chewwt <ruthchew35@gmail.com>
2016-10-05 17:19:07 +0800

    add Task class

--- task-tracker/src/main/data/Task.java
+++ task-tracker/src/main/data/Task.java
@@ -1,5 +1,33 @@
 package main.data;
 
-public class Task {
+import java.util.Objects;
 
+public class Task {
+    private String message;
+    
+    public Task(String message) {
+        this.message = message;
+    }
+    
+    public String getMessage(){
+        return this.message;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        return other == this 
+                || (other instanceof Task 
+                && this.message.equals(((Task) other).message));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+    
+    @Override
+    public String toString() {
+        return getMessage();
+    }    
+    
 }
