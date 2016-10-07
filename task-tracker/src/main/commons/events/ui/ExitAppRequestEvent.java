be84a6379016aecff27f432cf37ee1e5783fb34f
Chandraanshu <Chandraanshu@gmail.com>
2016-10-07 18:02:56 +0800

    copy commons from addressbook4

---
+++ task-tracker/src/main/commons/events/ui/ExitAppRequestEvent.java
@@ -0,0 +1,14 @@
+package main.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+
+/**
+ * Indicates a request for App termination
+ */
+public class ExitAppRequestEvent extends BaseEvent {
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
---
+++ task-tracker/src/main/commons/events/ui/IncorrectCommandAttemptedEvent.java
@@ -0,0 +1,18 @@
+package main.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.logic.commands.Command;
+
+/**
+ * Indicates an attempt to execute an incorrect command
+ */
+public class IncorrectCommandAttemptedEvent extends BaseEvent {
+
+    public IncorrectCommandAttemptedEvent(Command command) {}
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+}
