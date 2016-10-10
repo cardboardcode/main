c6117516eaa41502907b76007ed745d8fbff5321
chewwt <ruthchew35@gmail.com>
2016-10-10 14:51:07 +0800

    implement logic interface

--- task-tracker/src/main/logic/Logic.java
+++ task-tracker/src/main/logic/Logic.java
@@ -1,36 +1,7 @@
 package main.logic;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import main.logic.command.CommandResult;
 
-import javafx.util.Pair;
-import main.data.Task;
-import main.logic.parser.MainParser;
-import main.logic.parser.timeParser;
-
-public class Logic {
-    
-    ArrayList<Task> tasksList;
-    MainParser parser;
-    
-    public Logic() {
-        tasksList = new ArrayList<Task>();
-        parser = new MainParser();
-    }
-    
-    /**
-     * processes the input
-     * 
-     * @return text to be displayed
-     */
-    public String process(String input) {
-        return parser.parse(input).execute().feedbackToUser;
-//        timeParser.extractTime_test(input);
-//        Pair<String,List<Date>> parsed= timeParser.extractTime(input);
-//        System.out.println(parsed.getValue());
-//        return parsed.getKey();
-        
-    }
-
+public interface Logic {
+    public CommandResult execute(String input);
 }
--- task-tracker/src/main/logic/LogicManager.java
+++ task-tracker/src/main/logic/LogicManager.java
@@ -5,8 +5,8 @@ import main.commons.core.ComponentManager;
 import main.commons.core.LogsCenter;
 import main.logic.command.Command;
 import main.logic.command.CommandResult;
-import main.logic.parser.Parser;
-import main.model.Model;
+import main.logic.parser.MainParser;
+import main.model.model.Model;
 import main.storage.Storage;
 
 import java.util.logging.Logger;
@@ -18,19 +18,20 @@ public class LogicManager extends ComponentManager implements Logic {
     private final Logger logger = LogsCenter.getLogger(LogicManager.class);
 
     private final Model model;
-    private final Parser parser;
+    private final MainParser parser;
 
     public LogicManager(Model model, Storage storage) {
         this.model = model;
-        this.parser = new Parser();
+        this.parser = new MainParser();
     }
 
     @Override
     public CommandResult execute(String commandText) {
         logger.info("----------------[USER COMMAND][" + commandText + "]");
-        Command command = parser.parseCommand(commandText);
+        Command command = parser.parse(commandText);
         command.setData(model);
         return command.execute();
+
     }
 
 //    @Override
