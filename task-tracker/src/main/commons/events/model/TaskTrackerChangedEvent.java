2d862f10a01cf78ba1cffd82fdd2024f4f151a98
Chandraanshu <Chandraanshu@gmail.com>
2016-10-10 15:29:51 +0800

    update ComponentManager and other bugs

--- task-tracker/src/main/commons/events/model/TaskTrackerChangedEvent.java
+++ task-tracker/src/main/commons/events/model/TaskTrackerChangedEvent.java
@@ -1,7 +1,7 @@
 package main.commons.events.model;
 
 import main.commons.events.BaseEvent;
-import main.model.ReadOnlyAddressBook;
+//import main.model.model.ReadOnlyTaskTracker;
 import main.model.model.ReadOnlyTaskTracker;
 
 /** Indicates the AddressBook in the model has changed*/
@@ -15,6 +15,6 @@ public class TaskTrackerChangedEvent extends BaseEvent {
 
     @Override
     public String toString() {
-        return "number of persons " + data.getTaskList().size() + ", number of tags " + data.getTagList().size();
+        return "number of tasks " + data.getTaskList().size();
     }
 }
