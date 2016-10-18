825495e96bb3379843f98858fa15a9422fd04417
cardboardcode <cardboardmails@gmail.com>
2016-10-19 02:45:20 +0800

    Refined ListStatistics UI component further. Component now updates according. Waiting for the codes from the rest to work properly.

--- task-tracker/src/main/java/main/ui/ListStatistics.java
+++ task-tracker/src/main/java/main/ui/ListStatistics.java
@@ -13,6 +13,8 @@ import main.commons.util.AppUtil;
 import main.commons.util.FxViewUtil;
 import main.logic.Logic;
 import main.model.Model;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -39,6 +41,12 @@ public class ListStatistics extends UiPart {
 	@FXML
 	private ImageView image;
 	
+	private static final String TODAY_TASK_MESSAGE = "No. of Task Due Today: ";
+	private static final String TOMORROW_TASK_MESSAGE = "No. of Task Due Tomorrow: ";
+	private static final String EVENT_TASK_MESSAGE = "No. of Events: ";
+	private static final String DEADLINE_TASK_MESSAGE = "No. of Deadlines: ";
+	private static final String ALL_TASK_MESSAGE = "Total: ";
+	
 	private Model model;
 	
 	private static Logic logic;
@@ -48,11 +56,16 @@ public class ListStatistics extends UiPart {
 	private static VBox mainPane;
 	
 	private static ListStatistics listDisplay;
-
+	
+	private StringProperty todaytaskNo = new SimpleStringProperty(""); 
+	private StringProperty tomorrowtaskNo  = new SimpleStringProperty("");
+	private StringProperty eventtaskNo  = new SimpleStringProperty("");
+	private StringProperty deadlinetaskNo  = new SimpleStringProperty("");
+	private StringProperty alltaskNo  = new SimpleStringProperty("");
+	
 	public static ListStatistics load(Stage primaryStage, AnchorPane placeHolder,Logic logic) {
 		listDisplay = UiPartLoader.loadUiPart(primaryStage, placeHolder, new ListStatistics()); 
 		ListStatistics.logic = logic;
-//		listDisplay.model = logic.getModel();
 		listDisplay.configure();
 		return listDisplay;
 	}
@@ -66,11 +79,9 @@ public class ListStatistics extends UiPart {
 	}
 
 	public void configure() {
-		
-		listDisplay = new ListStatistics();
 		mainPane = new VBox();
-		
-		initialize();
+		bindingAllStringProperty();
+		initializeStringProperty();
 		setListIcon();
 		mainPane.getChildren().addAll(image, todaytasks, tomorrowtasks, eventtasks, deadlinetasks,alltasks);
 		mainPane.setSpacing(30.0);
@@ -79,35 +90,61 @@ public class ListStatistics extends UiPart {
 		
 	}
 
+	private void bindingAllStringProperty() {
+		todaytasks.textProperty().bind(todaytaskNo);
+		tomorrowtasks.textProperty().bind(tomorrowtaskNo);
+		eventtasks.textProperty().bind(eventtaskNo);
+		deadlinetasks.textProperty().bind(deadlinetaskNo);
+		alltasks.textProperty().bind(alltaskNo);
+	}
+
 	private void setListIcon() {
 		image = new ImageView(AppUtil.getImage("/images/statistics.png"));
 	}
-
-	private void initialize() {
-
-//		todaytasks.setText("No. of Task Due Today: " + model.getNumToday());
-//		tomorrowtasks.setText("No. of Task Due Tomorrow: " + model.getNumTmr());
-//		eventtasks.setText("No. of Events: " + model.getNumEvents());
-//		deadlinetasks.setText("No. of Deadlines: " + model.getNumDeadline());
-		todaytasks.setText("No. of Task Due Today: ");
-		tomorrowtasks.setText("No. of Task Due Tomorrow: ");
-		eventtasks.setText("No. of Events: ");
-		deadlinetasks.setText("No. of Deadlines: ");
-		alltasks.setText("Total: " + logic.getFilteredTaskList().size());
-
+	
+	/**
+	 * Remember to change both methods below on Wednesday when you meeet up to recombine.
+	 * You would not be using logic to get the data. You will have to use the model component directly. Wait for Ruth
+	 */
+	private void initializeStringProperty() {
+		todaytaskNo.setValue(TODAY_TASK_MESSAGE + logic.getFilteredTaskList().size());
+		tomorrowtaskNo.setValue(TOMORROW_TASK_MESSAGE + logic.getFilteredTaskList().size());
+		eventtaskNo.setValue(EVENT_TASK_MESSAGE + logic.getFilteredTaskList().size());
+		deadlinetaskNo.setValue(DEADLINE_TASK_MESSAGE + logic.getFilteredTaskList().size());
+		alltaskNo.setValue(ALL_TASK_MESSAGE + logic.getFilteredTaskList().size());
 	}
 	
 	public static void updateStatistics(){
-//		listDisplay.getAllTasksLabel().setText("No. of Total: "+listDisplay.getLogic().getFilteredTaskList().size());
-		System.out.println(logic.getFilteredTaskList().size());
-		Label newalltasks = new Label();
-		newalltasks.setText("Total: "+logic.getFilteredTaskList().size());
-		System.out.println(mainPane==null);
-		mainPane.getChildren().set(5, newalltasks);
+		listDisplay.getTodayTaskNo().setValue(TODAY_TASK_MESSAGE+logic.getFilteredTaskList().size());
+		listDisplay.getTomorrowTaskNo().setValue(TOMORROW_TASK_MESSAGE+logic.getFilteredTaskList().size());
+		listDisplay.getEventTaskNo().setValue(EVENT_TASK_MESSAGE+logic.getFilteredTaskList().size());
+		listDisplay.getDeadlineTaskNo().setValue(DEADLINE_TASK_MESSAGE+logic.getFilteredTaskList().size());
+		listDisplay.getAllTaskNo().setValue(ALL_TASK_MESSAGE+logic.getFilteredTaskList().size());
+		
+	}
+	
+	public StringProperty getTodayTaskNo(){
+		return todaytaskNo;
+	}
+	
+	public StringProperty getTomorrowTaskNo(){
+		return tomorrowtaskNo;
+	}
+	
+	public StringProperty getEventTaskNo(){
+		return eventtaskNo;
+	}
+	
+	public StringProperty getDeadlineTaskNo(){
+		return deadlinetaskNo;
 	}
 	
 	public Label getAllTasksLabel(){
 		return alltasks;
+	}
+	
+	public StringProperty getAllTaskNo(){
+		return alltaskNo;
 	}
 		
 	@Override
