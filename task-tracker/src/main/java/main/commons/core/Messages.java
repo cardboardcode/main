674f551ad107120d95811f50d725231275578bab
chewwt <ruthchew35@gmail.com>
2016-10-20 21:16:45 +0800

    add invalid list command message

--- task-tracker/src/main/java/main/commons/core/Messages.java
+++ task-tracker/src/main/java/main/commons/core/Messages.java
@@ -11,5 +11,6 @@ public class Messages {
     public static final String MESSAGE_TASK_LISTED_OVERVIEW = "%1$d tasks listed!";
     public static final String MESSAGE_EMPTY_DESCRIPTION = "Task description must be provided";
     public static final String MESSAGE_MULTIPLE_PRIORITY = "Task can only have 1 priority level";
-
+    public static final String MESSAGE_INVALID_PARAMETERS = "Invalid %1$s parameters! \n %2$s";
+    
 }
