a233dc589769da6954511354e0acbf954a8e7b57
Chandraanshu <Chandraanshu@gmail.com>
2016-10-22 23:59:06 +0800

    update undo command

--- task-tracker/src/main/java/main/logic/command/UndoCommand.java
+++ task-tracker/src/main/java/main/logic/command/UndoCommand.java
@@ -16,28 +16,32 @@ public class UndoCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Reverts the last known command input.\n" + "eg. "
             + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Reverted last known command: ";
-    public static final String MESSAGE_EMPTY_HISTORY = "There is no more inputs before this.";
+    public static final String MESSAGE_SUCCESS = "Reverted last command: ";
+    public static final String MESSAGE_EMPTY_HISTORY = "There are no more inputs before this.";
 
-    private String lastInput;
+    public static final int ADD = 1;
+    public static final int DEL = 2;
+    public static final int EDIT = 3;
+    public static final int DONE = 4;
 
-    public UndoCommand(String lastInput) {
-        this.lastInput = lastInput;
+    public UndoCommand() {
+
     }
 
     @Override
     public CommandResult execute() {
-        if (lastInput != null)
-            // Process
-            /**
-             * Add - Delete, Delete - Add, Edit - Delete & Add, Clear - AddAll
-             * 
-             */
-            return new CommandResult(MESSAGE_SUCCESS);
-        else
-            return new CommandResult("never gonna give you up, never gonna let you down,"
-                    + "never gonna run around and desert you. Never gonna make you "
-                    + "cry, never gonna say goodbye. Never gonna tell a lie and hurt you.");
+        return null;
+//        if (lastInput != null)
+//            // Process
+//            /**
+//             * Add - Delete, Delete - Add, Edit - Delete & Add, Clear - AddAll
+//             * 
+//             */
+//            return new CommandResult(MESSAGE_SUCCESS);
+//        else
+//            return new CommandResult("never gonna give you up, never gonna let you down,"
+//                    + "never gonna run around and desert you. Never gonna make you "
+//                    + "cry, never gonna say goodbye. Never gonna tell a lie and hurt you.");
     }
 
 }
