c7e16af3a7ee3227747870a25e4e4133703f9d1c
darylgqy <darylgqy@gmail.com>
2016-10-23 14:07:42 +0800

    Added Status class(Done, Undone)

---
+++ task-tracker/src/main/java/main/model/task/Status.java
@@ -0,0 +1,51 @@
+package main.model.task;
+
+public class Status {
+	public static enum State{
+		DONE, UNDONE
+	}
+	private static final String MESSAGE_TASK_DONE = "DONE";
+	private static final String MESSAGE_TASK_UNDONE = "UNDONE";
+	private State status;
+	
+	public Status(State status){
+		assert status !=null;
+		this.status = status;
+	}
+	
+	public Status(String statusString){
+		assert statusString != null;
+		this.status = getStatusString(statusString);
+	}
+	
+	
+	
+	private State getStatusString(String statusString){
+		
+		if(statusString.equals(MESSAGE_TASK_DONE)){
+			return State.DONE;
+		}
+		else{
+			return State.UNDONE;
+		}
+	}
+	
+	@Override
+	public boolean equals(Object other){
+		return other == this || //same object return true
+				 (other instanceof Status// null
+				&& this.status.equals(((Status) other).status));
+	}
+	@Override
+	public String toString(){
+		String statusString;
+		
+		if(status == State.DONE){
+			return statusString = "Done";
+		}
+		else{
+			return statusString = "Undone";
+		}
+	}
+	
+}
