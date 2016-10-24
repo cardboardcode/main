0289b54ea8aef7da5aea82527a93b31f7fc662e7
cardboardcode <cardboardmails@gmail.com>
2016-10-24 15:51:21 +0800

    Added SorteList implementation in TestUtil. Error AddCommand not fixed yet.

--- task-tracker/src/test/java/guitests/guihandles/TaskListPanelHandle.java
+++ task-tracker/src/test/java/guitests/guihandles/TaskListPanelHandle.java
@@ -70,6 +70,8 @@ public class TaskListPanelHandle extends GuiHandle {
         // Return false if any of the tasks doesn't match
         for (int i = 0; i < tasks.length - 1; i++) {
             if (!tasksInList.get(startPosition + i).getMessage().equals(tasks[i].getMessage())){
+            	System.out.println(tasksInList.get(startPosition + i).getMessage());
+            	System.out.println(tasks[i].getMessage());
                 return false;
             }
         }
