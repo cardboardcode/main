aaab5768b4cc3c5989224abf91526ccfb408a12b
chewwt <ruthchew35@gmail.com>
2016-10-25 02:08:30 +0800

    add nicer list message

--- task-tracker/src/main/java/main/logic/command/ListCommand.java
+++ task-tracker/src/main/java/main/logic/command/ListCommand.java
@@ -4,11 +4,12 @@ package main.logic.command;
 import java.util.Date;
 import org.apache.commons.lang3.tuple.Triple;
 
+import main.commons.util.DateUtil;
 import main.model.task.PriorityType;
 import main.model.task.TaskType;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists certain tasks in the task tracker to the user.
  */
 
 public class ListCommand extends Command {
@@ -18,7 +19,7 @@ public class ListCommand extends Command {
             + "Parameters: [priority] [date] [type] \n"
             + "All parameters optional and interchangeable \n"
             + "Eg: " + COMMAND_WORD + " high today";
-    public static final String MESSAGE_SUCCESS = "Listed all tasks";
+    public static final String MESSAGE_SUCCESS = "Listed all %1$s";
     
     PriorityType priority;
     Date date;
@@ -38,9 +39,29 @@ public class ListCommand extends Command {
 	@Override
 	public CommandResult execute() {
 	       
-	    model.updateFilteredListToShowAll();
+	    model.updateFilteredListToShowAllPending();
 	    if (!toShowAll) model.updateFilteredTaskList(Triple.of(priority, date, type));
 	    
-	    return new CommandResult(MESSAGE_SUCCESS);    
+	    return new CommandResult(String.format(MESSAGE_SUCCESS, getReadableCriteria()));    
+	}
+	
+	private String getReadableCriteria() {
+	    StringBuilder readable = new StringBuilder();
+	    
+	    String task;
+	    if (type == null) task = "tasks";
+	    else if (type == TaskType.EVENT) task = "events";
+        else if (type == TaskType.DEADLINE) task = "tasks with deadlines";
+        else task = "floating tasks";
+	    
+	    if (priority == null) readable.append(task);
+	    else if (priority == PriorityType.LOW) readable.append("low priority ").append(task);
+	    else if (priority == PriorityType.HIGH) readable.append("high priority ").append(task);
+	    else readable.append("normal priority ").append(task);
+
+	    if (date != null) readable.append(" due ").append(DateUtil.readableDate(date));
+	    
+	    return readable.toString();
+	    
 	}
 }
