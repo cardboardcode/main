551d592baa29c02bf6f36320560be16b1416a928
chewwt <ruthchew35@gmail.com>
2016-10-25 05:52:52 +0800

    add new events

--- task-tracker/src/main/java/main/commons/events/ui/TabPressEvent.java
+++ task-tracker/src/main/java/main/commons/events/ui/TabPressEvent.java
@@ -1,30 +1,23 @@
-//@@author A0144132W
 package main.commons.events.ui;
 
 import javafx.scene.input.KeyCode;
 import main.commons.events.BaseEvent;
 
-public class KeyPressEvent extends BaseEvent{
-    private String input;
-    private KeyCode button;
-
-    public KeyPressEvent(KeyCode button, String input) {
-        this.button = button;
-        this.input = input;
-    }
+public class TabPressEvent extends BaseEvent{
     
+    private KeyCode button;
+    
+    public TabPressEvent(KeyCode button) {
+        this.button = button;
+    }
+
     public KeyCode getKey() {
         return button;
     }
     
-    public String getInput() {
-        return input;
-    }
-
     @Override
     public String toString() {
         return this.getClass().getSimpleName();
     }
-    
 
 }
