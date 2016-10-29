d68e2a2ac1f4762212bb809cce7c30e6c7c6525e
Chandraanshu <Chandraanshu@gmail.com>
2016-10-30 00:02:32 +0800

    added partial done test

---
+++ task-tracker/src/test/java/guitests/DoneCommandTest.java
@@ -0,0 +1,27 @@
+//@@author A0142686X    
+package guitests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.commons.core.Messages;
+import main.logic.command.DoneCommand;
+import main.model.task.ReadOnlyTask;
+import main.testutil.TestTask;
+
+public class DoneCommandTest extends TaskTrackerGuiTest {
+    
+    TestTask[] currentList = td.getTypicalTasks();
+    int targetIndex = 1;
+    
+    @Test
+    public void doneTest() {
+        commandBox.runCommand("done " + (currentList.length + 1));
+        assertResultMessage("Task does not exist in task-tracker");
+        
+//        ReadOnlyTask doneTask = taskListPanel.getTask(targetIndex - 1);
+//        commandBox.runCommand("done " + targetIndex );        
+//        assertTrue(doneTask.getIsDone());
+    }
+}
