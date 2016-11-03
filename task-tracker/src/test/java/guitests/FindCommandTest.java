044ad2529ba471e2957c8df1d346b4bb5599709b
cardboardcode <cardboardmails@gmail.com>
2016-11-04 02:34:33 +0800

    #88 Added FindCommandTest.java but only test find command result display

---
+++ task-tracker/src/test/java/guitests/FindCommandTest.java
@@ -0,0 +1,15 @@
+package guitests;
+
+import org.junit.Test;
+
+import main.logic.command.FindCommand;
+
+public class FindCommandTest extends TaskTrackerGuiTest{
+	
+	@Test
+	public void find(){
+		//Find command does not run on GuiTests
+		commandBox.runCommand("find" + " friends");
+		assertResultMessage(String.format(FindCommand.MESSAGE_SUCCESS,6));
+	}
+}
