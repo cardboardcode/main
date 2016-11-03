ef629c07d2e3634b4eb544eb7681dc796b0b09f6
cardboardcode <cardboardmails@gmail.com>
2016-11-04 02:03:25 +0800

    #88 Added guitest for SortCommand : SortCommandTest.java added

---
+++ task-tracker/src/test/java/guitests/SortCommandTest.java
@@ -0,0 +1,27 @@
+//@@author A0139422J
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import main.testutil.TestTask;
+import main.testutil.TestUtil;
+
+public class SortCommandTest extends TaskTrackerGuiTest {
+
+	@Test
+	public void sort() {
+		TestTask[] currentList = td.getTypicalTasks();
+
+		// Sorts the list by name
+		commandBox.runCommand("sort" + " name");
+		TestTask[] expectedList = TestUtil.sortTasksByName(currentList);
+		assertTrue(taskListPanel.isListMatching(expectedList));
+
+		// Sorts the list by date
+		commandBox.runCommand("sort" + " date");
+		expectedList = TestUtil.sortTasksByDate(currentList);
+		assertTrue(taskListPanel.isListMatching(expectedList));
+	}
+}
