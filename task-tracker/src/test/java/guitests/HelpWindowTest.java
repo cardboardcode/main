421839d84050051dc826133d86b1ee3247946025
cardboardcode <cardboardmails@gmail.com>
2016-11-04 02:44:09 +0800

    #88 Added gui test for help window

---
+++ task-tracker/src/test/java/guitests/HelpWindowTest.java
@@ -0,0 +1,22 @@
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import guitests.guihandles.HelpWindowHandle;
+
+public class HelpWindowTest extends TaskTrackerGuiTest{
+	
+	@Test
+    public void openHelpWindow() {
+
+        assertHelpWindowOpen(commandBox.runHelpCommand());
+
+    }
+
+    private void assertHelpWindowOpen(HelpWindowHandle helpWindowHandle) {
+        assertTrue(helpWindowHandle.isWindowOpen());
+        helpWindowHandle.closeWindow();
+    }
+}
