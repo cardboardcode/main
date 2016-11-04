7cc51458d62a920ca0ec831f3600cd1d736d8797
chewwt <ruthchew35@gmail.com>
2016-11-05 00:48:52 +0800

    add file path changed event

---
+++ task-tracker/src/main/java/main/commons/events/storage/FilePathChangedEvent.java
@@ -0,0 +1,27 @@
+package main.commons.events.storage;
+
+import main.commons.events.BaseEvent;
+import main.model.ReadOnlyTaskTracker;
+
+public class FilePathChangedEvent extends BaseEvent{
+    String filePath;
+    ReadOnlyTaskTracker taskTracker;
+
+    public FilePathChangedEvent(String filePath, ReadOnlyTaskTracker taskTracker) {
+        this.filePath = filePath;
+        this.taskTracker = taskTracker;
+    }
+    
+    public String getFilePath() {
+        return filePath;
+    }
+    
+    public ReadOnlyTaskTracker getTaskTracker() {
+        return taskTracker;
+    }
+
+    @Override
+    public String toString() {
+        return filePath;
+    }
+}
