e6d7b61f55fd3b7fa9b8ce6de99f91fd944f9439
cardboardcode <cardboardmails@gmail.com>
2016-11-06 13:57:26 +0800

    DoneCommand R

--- task-tracker/src/main/java/main/logic/command/DoneCommand.java
+++ task-tracker/src/main/java/main/logic/command/DoneCommand.java
@@ -8,8 +8,8 @@ public class DoneCommand extends Command{
 	public static final String COMMAND_WORD = "done";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Indicates a task to be done\n"
-            + "Parameters: <task index>\n"
-            + "Eg: " + COMMAND_WORD + " 1";
+            												+ "Parameters: <task index>\n"
+            												+ "Eg: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_SUCCESS = "The following task is done: %1$s";
     public static final String MESSAGE_TASK_ALREADY_DONE = "This task is already marked as done";
@@ -28,11 +28,9 @@ public class DoneCommand extends Command{
         try {
             model.doneTask(taskIndex);
             return new CommandResult(String.format(MESSAGE_SUCCESS, taskIndex+1));
-        } 
-        catch (IndexOutOfBoundsException ie) {
+        } catch (IndexOutOfBoundsException ie) {
             return new CommandResult("The task index provided is invalid");
-        } 
-        catch (TaskNotFoundException pnfe) {
+        } catch (TaskNotFoundException pnfe) {
             return new CommandResult("Task does not exist in task-tracker");
         }
     }
