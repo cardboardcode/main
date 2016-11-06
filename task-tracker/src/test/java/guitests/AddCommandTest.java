622471057acd8db9bd5777d4b8ff019d7c322152
cardboardcode <cardboardmails@gmail.com>
2016-11-06 14:46:20 +0800

    AddCommandTest R

--- task-tracker/src/test/java/guitests/AddCommandTest.java
+++ task-tracker/src/test/java/guitests/AddCommandTest.java
@@ -2,54 +2,49 @@
 package guitests;
 
 import guitests.guihandles.TaskCardHandle;
-import main.commons.core.Messages;
 import main.logic.command.AddCommand;
 import main.testutil.TestTask;
 import main.testutil.TestUtil;
 import main.testutil.TypicalTestTasks;
-
 import org.junit.Test;
-
-
 import static org.junit.Assert.assertTrue;
 
 public class AddCommandTest extends TaskTrackerGuiTest {
 
-    @Test
-    public void add() {
-//        //add one task
-        TestTask[] currentList = td.getTypicalTasks();
+	@Test
+	public void add() {
+		// add one task
+		TestTask[] currentList = td.getTypicalTasks();
 
-//        //add another task
-        TestTask taskToAdd = TypicalTestTasks.floating3;
-        assertAddSuccess(taskToAdd, currentList);
-        currentList = TestUtil.addTasksToList(currentList, taskToAdd);
-        
-////        //add duplicate floating task
-        commandBox.runCommand(TypicalTestTasks.floating2.getAddCommand());
-        assertResultMessage(AddCommand.MESSAGE_DUPLICATE_TASK);
-        assertTrue(taskListPanel.isListMatching(currentList));
-        
-//        //add to empty list
-        commandBox.runCommand("clear");
-        assertAddSuccess(TypicalTestTasks.floating1);
+		// add another task
+		TestTask taskToAdd = TypicalTestTasks.floating3;
+		assertAddSuccess(taskToAdd, currentList);
+		currentList = TestUtil.addTasksToList(currentList, taskToAdd);
 
-//        
-//        //invalid command
-        commandBox.runCommand("adds wrongcommandinput");
-        assertResultMessage("Invalid command format! \nUnknown command");
-    }
+		// add duplicate floating task
+		commandBox.runCommand(TypicalTestTasks.floating2.getAddCommand());
+		assertResultMessage(AddCommand.MESSAGE_DUPLICATE_TASK);
+		assertTrue(taskListPanel.isListMatching(currentList));
 
-    private void assertAddSuccess(TestTask taskToAdd, TestTask... currentList) {
-        commandBox.runCommand(taskToAdd.getAddCommand());
+		// add to empty list
+		commandBox.runCommand("clear");
+		assertAddSuccess(TypicalTestTasks.floating1);
 
-        //confirm the new card contains the right data
-        TaskCardHandle addedCard = taskListPanel.navigateToTask(taskToAdd.getMessage());
-        assertMatching(taskToAdd, addedCard);
+		// invalid command
+		commandBox.runCommand("adds wrongcommandinput");
+		assertResultMessage("Invalid command format! \nUnknown command");
+	}
 
-        //confirm the list now contains all previous persons plus the new person
-        TestTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
-        assertTrue(taskListPanel.isListMatching(expectedList));
-    }
+	private void assertAddSuccess(TestTask taskToAdd, TestTask... currentList) {
+		commandBox.runCommand(taskToAdd.getAddCommand());
+
+		// confirm the new card contains the right data
+		TaskCardHandle addedCard = taskListPanel.navigateToTask(taskToAdd.getMessage());
+		assertMatching(taskToAdd, addedCard);
+
+		// confirm the list now contains all previous tasks plus the new task
+		TestTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
+		assertTrue(taskListPanel.isListMatching(expectedList));
+	}
 
 }
