8fcb08fee87bf1ed2038d4737252267327b9965f
cardboardcode <cardboardmails@gmail.com>
2016-11-06 14:00:25 +0800

    ExitCommand R

--- task-tracker/src/main/java/main/logic/command/ExitCommand.java
+++ task-tracker/src/main/java/main/logic/command/ExitCommand.java
@@ -2,34 +2,28 @@
 package main.logic.command;
 
 import main.commons.core.EventsCenter;
-
 import main.commons.events.ui.ExitAppRequestEvent;
 import main.logic.command.CommandResult;
 
 /**
  * Terminates the program.
- * EventsCenter is from commons and ExitApp is in UI. Remember to work on them.
- * @author bey
  */
+public class ExitCommand extends Command {
 
-public class ExitCommand extends Command{
-	
 	public static final String COMMAND_WORD = "exit";
 
-	    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Tracker-Tracker as requested ...";
+	public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Tracker-Tracker as requested ...";
 
-		public static final String MESSAGE_USAGE = COMMAND_WORD + ": exits Task-Tracker";
- 
-		public ExitCommand() {
-			super();
-		}
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ": exits Task-Tracker";
 
+	public ExitCommand() {
+		super();
+	}
 
-	    @Override
-	    //
-	    public CommandResult execute() {
-	        EventsCenter.getInstance().post(new ExitAppRequestEvent());
-	        return new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT);
-	    }
-	
+	@Override
+	public CommandResult execute() {
+		EventsCenter.getInstance().post(new ExitAppRequestEvent());
+		return new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT);
+	}
+
 }
