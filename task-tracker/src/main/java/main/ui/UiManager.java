84b11d65a6c383706a0ea13aacdc4a1cad977dec
cardboardcode <cardboardmails@gmail.com>
2016-11-06 14:32:12 +0800

    UiManager R

--- task-tracker/src/main/java/main/ui/UiManager.java
+++ task-tracker/src/main/java/main/ui/UiManager.java
@@ -22,108 +22,104 @@ import java.util.logging.Logger;
 
 /**
  * The manager of the UI component.
- * "person" keyword check done
- * "addressbook" keyword check done
- * @author bey
  */
 public class UiManager extends ComponentManager implements Ui {
-    private static final Logger logger = LogsCenter.getLogger(UiManager.class);
-    private static final String ICON_APPLICATION = "/images/pp.png";
+	private static final Logger logger = LogsCenter.getLogger(UiManager.class);
+	private static final String ICON_APPLICATION = "/images/pp.png";
 
-    private Logic logic;
-    private Config config;
-    private UserPrefs prefs;
-    private MainWindow mainWindow;
+	private Logic logic;
+	private Config config;
+	private UserPrefs prefs;
+	private MainWindow mainWindow;
 
-    public UiManager(Logic logic, Config config, UserPrefs prefs) {
-        super();
-        this.logic = logic;
-        this.config = config;
-        this.prefs = prefs;
-    }
+	public UiManager(Logic logic, Config config, UserPrefs prefs) {
+		super();
+		this.logic = logic;
+		this.config = config;
+		this.prefs = prefs;
+	}
 
-    @Override
-    public void start(Stage primaryStage) {
-        logger.info("Starting UI...");
-        primaryStage.setTitle(config.getAppTitle());
+	@Override
+	public void start(Stage primaryStage) {
+		logger.info("Starting UI...");
+		primaryStage.setTitle(config.getAppTitle());
 
-        //Set the application icon.
-        primaryStage.getIcons().add(getImage(ICON_APPLICATION));
+		// Set the application icon.
+		primaryStage.getIcons().add(getImage(ICON_APPLICATION));
 
-        try {
-            mainWindow = MainWindow.load(primaryStage, config, prefs, logic);
-            mainWindow.show(); //This should be called before creating other UI parts
-            mainWindow.fillInnerParts();
+		try {
+			mainWindow = MainWindow.load(primaryStage, config, prefs, logic);
+			mainWindow.show(); 
+			mainWindow.fillInnerParts();
 
-        } catch (Throwable e) {
-            logger.severe(StringUtil.getDetails(e));
-            showFatalErrorDialogAndShutdown("Fatal error during initializing", e);
-        }
-    }
+		} catch (Throwable e) {
+			logger.severe(StringUtil.getDetails(e));
+			showFatalErrorDialogAndShutdown("Fatal error during initializing", e);
+		}
+	}
 
-    @Override
-    public void stop() {
-        prefs.updateLastUsedGuiSetting(mainWindow.getCurrentGuiSetting());
-        mainWindow.hide();
-        mainWindow.closeHelpWindow();
-    }
+	@Override
+	public void stop() {
+		prefs.updateLastUsedGuiSetting(mainWindow.getCurrentGuiSetting());
+		mainWindow.hide();
+		mainWindow.closeHelpWindow();
+	}
 
-    private void showFileOperationAlertAndWait(String description, String details, Throwable cause) {
-        final String content = details + ":\n" + cause.toString();
-        showAlertDialogAndWait(AlertType.ERROR, "File Op Error", description, content);
-    }
+	private void showFileOperationAlertAndWait(String description, String details, Throwable cause) {
+		final String content = details + ":\n" + cause.toString();
+		showAlertDialogAndWait(AlertType.ERROR, "File Op Error", description, content);
+	}
 
-    private Image getImage(String imagePath) {
-        return new Image(Main.class.getResourceAsStream(imagePath));
-    }
+	private Image getImage(String imagePath) {
+		return new Image(Main.class.getResourceAsStream(imagePath));
+	}
 
-    void showAlertDialogAndWait(Alert.AlertType type, String title, String headerText, String contentText) {
-        showAlertDialogAndWait(mainWindow.getPrimaryStage(), type, title, headerText, contentText);
-    }
+	void showAlertDialogAndWait(Alert.AlertType type, String title, String headerText, String contentText) {
+		showAlertDialogAndWait(mainWindow.getPrimaryStage(), type, title, headerText, contentText);
+	}
 
-    private static void showAlertDialogAndWait(Stage owner, AlertType type, String title, String headerText,
-                                               String contentText) {
-        final Alert alert = new Alert(type);
-        alert.getDialogPane().getStylesheets().add("view/DarkTheme.css");
-        alert.initOwner(owner);
-        alert.setTitle(title);
-        alert.setHeaderText(headerText);
-        alert.setContentText(contentText);
+	private static void showAlertDialogAndWait(Stage owner, AlertType type, String title, String headerText,
+			String contentText) {
+		final Alert alert = new Alert(type);
+		alert.getDialogPane().getStylesheets().add("view/DarkTheme.css");
+		alert.initOwner(owner);
+		alert.setTitle(title);
+		alert.setHeaderText(headerText);
+		alert.setContentText(contentText);
 
-        alert.showAndWait();
-    }
+		alert.showAndWait();
+	}
 
-    private void showFatalErrorDialogAndShutdown(String title, Throwable e) {
-        logger.severe(title + " " + e.getMessage() + StringUtil.getDetails(e));
-        showAlertDialogAndWait(Alert.AlertType.ERROR, title, e.getMessage(), e.toString());
-        Platform.exit();
-        System.exit(1);
-    }
+	private void showFatalErrorDialogAndShutdown(String title, Throwable e) {
+		logger.severe(title + " " + e.getMessage() + StringUtil.getDetails(e));
+		showAlertDialogAndWait(Alert.AlertType.ERROR, title, e.getMessage(), e.toString());
+		Platform.exit();
+		System.exit(1);
+	}
 
-    //==================== Event Handling Code =================================================================
+	// ==================== Event Handling Code =================================================================//
 
-    @Subscribe
-    private void handleDataSavingExceptionEvent(DataSavingExceptionEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        showFileOperationAlertAndWait("Could not save data", "Could not save data to file", event.exception);
-    }
+	@Subscribe
+	private void handleDataSavingExceptionEvent(DataSavingExceptionEvent event) {
+		logger.info(LogsCenter.getEventHandlingLogMessage(event));
+		showFileOperationAlertAndWait("Could not save data", "Could not save data to file", event.exception);
+	}
 
-    @Subscribe
-    private void handleShowHelpEvent(ShowHelpRequestEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        mainWindow.handleHelp();
-    }
+	@Subscribe
+	private void handleShowHelpEvent(ShowHelpRequestEvent event) {
+		logger.info(LogsCenter.getEventHandlingLogMessage(event));
+		mainWindow.handleHelp();
+	}
 
-    @Subscribe
-    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        mainWindow.getTaskListPanel().scrollTo(event.targetIndex);
-    }
+	@Subscribe
+	private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
+		logger.info(LogsCenter.getEventHandlingLogMessage(event));
+		mainWindow.getTaskListPanel().scrollTo(event.targetIndex);
+	}
 
-    @Subscribe
-    private void handleTaskPanelSelectionChangedEvent(TaskPanelSelectionChangedEvent event){
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-//        mainWindow.loadTaskPage(event.getNewSelection());
-    }
+	@Subscribe
+	private void handleTaskPanelSelectionChangedEvent(TaskPanelSelectionChangedEvent event) {
+		logger.info(LogsCenter.getEventHandlingLogMessage(event));
+	}
 
 }
