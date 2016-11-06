37f98f8179afb84d88204ebea8c787959b58e813
cardboardcode <cardboardmails@gmail.com>
2016-11-06 14:53:58 +0800

    DeleteCommandTest R

--- task-tracker/src/test/java/guitests/DeleteCommandTest.java
+++ task-tracker/src/test/java/guitests/DeleteCommandTest.java
@@ -2,53 +2,67 @@
 package guitests;
 
 import org.junit.Test;
-
 import main.testutil.TestTask;
 import main.testutil.TestUtil;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Evaluates the delete command when it deletes the first in the list, the last
+ * in the list, the middle in the list as well as when it deletes an invalid
+ */
 public class DeleteCommandTest extends TaskTrackerGuiTest {
 
-    @Test
-    public void delete() {
+	@Test
+	public void delete() {
 
-        //delete the first in the list
-        TestTask[] currentList = td.getTypicalTasks();
-        int targetIndex = 1;
-        assertDeleteSuccess(targetIndex, currentList);
+		// delete the first in the list
+		TestTask[] currentList = td.getTypicalTasks();
+		int targetIndex = 1;
+		assertDeleteSuccess(targetIndex, currentList);
 
-        //delete the last in the list
-        currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
-        targetIndex = currentList.length;
-        assertDeleteSuccess(targetIndex, currentList);
+		// delete the last in the list
+		currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
+		targetIndex = currentList.length;
+		assertDeleteSuccess(targetIndex, currentList);
 
-        //delete from the middle of the list
-        currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
-        targetIndex = currentList.length/2;
-        assertDeleteSuccess(targetIndex, currentList);
+		// delete from the middle of the list
+		currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
+		targetIndex = currentList.length / 2;
+		assertDeleteSuccess(targetIndex, currentList);
 
-        //invalid index
-        commandBox.runCommand("delete " + currentList.length + 1);
-        assertResultMessage("Task does not exist in Task-Tracker");
+		// invalid index
+		commandBox.runCommand("delete " + currentList.length + 1);
+		assertResultMessage("Task does not exist in Task-Tracker");
 
-    }
+	}
 
-    /**
-     * Runs the delete command to delete the task at specified index and confirms the result is correct.
-     * @param targetIndexOneIndexed e.g. to delete the first task in the list, 1 should be given as the target index.
-     * @param currentList A copy of the current list of tasks (before deletion).
-     */
-    private void assertDeleteSuccess(int targetIndexOneIndexed, final TestTask[] currentList) {
-        TestTask taskToDelete = currentList[targetIndexOneIndexed-1]; //-1 because array uses zero indexing
-        TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, targetIndexOneIndexed);
+	/**
+	 * Runs the delete command to delete the task at specified index and
+	 * confirms the result is correct.
+	 * 
+	 * @param targetIndexOneIndexed
+	 *            e.g. to delete the first task in the list, 1 should be given
+	 *            as the target index.
+	 * @param currentList
+	 *            A copy of the current list of tasks (before deletion).
+	 */
+	private void assertDeleteSuccess(int targetIndexOneIndexed, final TestTask[] currentList) {
+		TestTask taskToDelete = currentList[targetIndexOneIndexed - 1]; // -1
+																		// because
+																		// array
+																		// uses
+																		// zero
+																		// indexing
+		TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, targetIndexOneIndexed);
 
-        commandBox.runCommand("delete " + targetIndexOneIndexed);
+		commandBox.runCommand("delete " + targetIndexOneIndexed);
 
-        //confirm the list now contains all previous tasks except the deleted person
-        assertTrue(taskListPanel.isListMatching(expectedRemainder));
+		// confirm the list now contains all previous tasks except the deleted
+		// task
+		assertTrue(taskListPanel.isListMatching(expectedRemainder));
 
-        //confirm the result message is correct
-        assertResultMessage(String.format("Deleted Task: " +targetIndexOneIndexed , taskToDelete));
-    }
+		// confirm the result message is correct
+		assertResultMessage(String.format("Deleted Task: " + targetIndexOneIndexed, taskToDelete));
+	}
 
 }
