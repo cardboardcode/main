cce12691d36c31a0b7507d3fe56e57f1e3869d5a
chewwt <ruthchew35@gmail.com>
2016-11-06 18:08:59 +0800

    add FindCommandTest

--- task-tracker/src/test/java/guitests/FindCommandTest.java
+++ task-tracker/src/test/java/guitests/FindCommandTest.java
@@ -2,6 +2,10 @@
 package guitests;
 
 import org.junit.Test;
+
+import javafx.scene.input.KeyCode;
+import main.commons.core.EventsCenter;
+import main.commons.events.ui.KeyPressEvent;
 import main.logic.command.FindCommand;
 
 /**
@@ -11,9 +15,38 @@ import main.logic.command.FindCommand;
 public class FindCommandTest extends TaskTrackerGuiTest {
 
 	@Test
-	public void find() {
-		// finds any tasks with the "friends" keyword
-		commandBox.runCommand("find" + " friends");
-		assertResultMessage(String.format(FindCommand.MESSAGE_SUCCESS, 6));
+	public void find_oneKeyword_success() {
+        // finds any tasks starting with the "frie"
+	    String input = "find" + " frie";
+	    
+		commandBox.enterCommand(input);
+		EventsCenter.getInstance().post(new KeyPressEvent(KeyCode.E, input));
+		commandBox.pressEnter();
+		
+		assertResultMessage(String.format(FindCommand.MESSAGE_SUCCESS, 1));
 	}
+	
+    @Test
+    public void find_twoKeyword_success() {
+        // finds any tasks starting with the "frie" or "b" 
+        String input = "find" + " frie" + " b";
+        
+        commandBox.enterCommand(input);
+        EventsCenter.getInstance().post(new KeyPressEvent(KeyCode.O, input));
+        commandBox.pressEnter();
+        
+        assertResultMessage(String.format(FindCommand.MESSAGE_SUCCESS, 3));
+    }
+    
+    @Test
+    public void find_middleParam_success() {
+        // finds any tasks starting with the "frie" or "b" 
+        String input = "find" + " with";
+        
+        commandBox.enterCommand(input);
+        EventsCenter.getInstance().post(new KeyPressEvent(KeyCode.H, input));
+        commandBox.pressEnter();
+        
+        assertResultMessage(String.format(FindCommand.MESSAGE_SUCCESS, 1));
+    }
 }
