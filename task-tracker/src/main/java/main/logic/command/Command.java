27201d26d8ca55fd309a98dbe793092d27237dd4
cardboardcode <cardboardmails@gmail.com>
2016-11-06 13:54:05 +0800

    Command again

--- task-tracker/src/main/java/main/logic/command/Command.java
+++ task-tracker/src/main/java/main/logic/command/Command.java
@@ -7,45 +7,48 @@ import main.model.Model;
 import main.storage.Storage;
 
 /**
- * Represents a command with hidden internal logic and the ability to be executed.
+ * Represents a command with hidden internal logic and the ability to be
+ * executed.
  */
 public abstract class Command {
 	protected Model model;
 	protected Storage storage;
 	private int targetIndex = -1;
-//	/**
-//     * @param targetIndex last visible listing index of the target task
-//     */
+
+	// /**
+	// * @param targetIndex last visible listing index of the target task
+	// */
 	public Command(int targetIndex) {
-        this.setTargetIndex(targetIndex);
-    }
-	
-	public Command() {}
-	
+		this.setTargetIndex(targetIndex);
+	}
+
+	public Command() {
+	}
+
 	public void setTargetIndex(int targetIndex) {
-        this.targetIndex = targetIndex;
-    }
-    /**
-     * Executes the command and returns the result message.
-     *
-     * @return feedback message of the operation result for display
-     */
-    public abstract CommandResult execute();
+		this.targetIndex = targetIndex;
+	}
 
-    /**
-     * Provides any needed dependencies to the command.
-     * Commands making use of any of these should override this method to gain
-     * access to the dependencies.
-     */
-    public void setData(Model model) {
-        this.model = model;
-    }
+	/**
+	 * Executes the command and returns the result message.
+	 *
+	 * @return feedback message of the operation result for display
+	 */
+	public abstract CommandResult execute();
 
-    /**
-     * Raises an event to indicate an attempt to execute an incorrect command
-     */
-    protected void indicateAttemptToExecuteIncorrectCommand() {
-        EventsCenter.getInstance().post(new IncorrectCommandAttemptedEvent(this));
-    }
-       
+	/**
+	 * Provides any needed dependencies to the command. Commands making use of
+	 * any of these should override this method to gain access to the
+	 * dependencies.
+	 */
+	public void setData(Model model) {
+		this.model = model;
+	}
+
+	/**
+	 * Raises an event to indicate an attempt to execute an incorrect command
+	 */
+	protected void indicateAttemptToExecuteIncorrectCommand() {
+		EventsCenter.getInstance().post(new IncorrectCommandAttemptedEvent(this));
+	}
 }
