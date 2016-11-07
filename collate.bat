71834eaaa19971728bd2c9bf460838ddc0568266
chewwt <ruthchew35@gmail.com>
2016-11-07 21:23:56 +0800

    remove collate jar

--- collate.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-
-java -jar Collate-TUI.jar collate from task-tracker/src/main to collated/main include java, fxml, css
-
-java -jar Collate-TUI.jar collate from task-tracker/src/test to collated/test include java
-
-java -jar Collate-TUI.jar collate from docs to collated/docs include md, html
--- collate.sh
+++
@@ -1,5 +0,0 @@
-java -jar Collate-TUI.jar collate from task-tracker/src/main to collated/main include java, fxml, css
-
-java -jar Collate-TUI.jar collate from task-tracker/src/test to collated/test include java
-
-java -jar Collate-TUI.jar collate from docs to collated/docs include md, html
